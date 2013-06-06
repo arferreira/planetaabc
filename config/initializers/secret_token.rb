edb2f9d5c21749ada3f066749a55bac4380d3acb
Antonio Ricardo <antonioricardo@ifollowagencia.com.br>
2013-06-06 02:50:21 -0400

    Primeiro Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Planetaabc::Application.config.secret_token = 'ee641174fcb9de5370cd4f6aa470dd3378a8b263b3271ef1b95b968990d00079bcbdc97760a10a1b54b086a98399141e56dc37595c89f3ac4e9187f8b1f863b2'
