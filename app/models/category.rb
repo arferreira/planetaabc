5f8df0a4e8d873ec5abaabac171e589fe999981c
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-08-03 20:36:30 -0300

    finalizando post do blog

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ActiveRecord::Base
   attr_accessible :name
   has_many :events
+  has_many :posts
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
   has_many :post_comments
+  belongs_to :category
   mount_uploader :picture, PictureUploader
   attr_accessible :author, :category_id, :content, :excerpt, :title, :picture
 end
