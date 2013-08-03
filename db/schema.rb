02488e0df9238040a2f3676528dd28831d5c2624
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-08-03 13:41:15 -0300

    admins

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130803160734) do
+ActiveRecord::Schema.define(:version => 20130803163756) do
 
   create_table "abouts", :force => true do |t|
     t.string   "title"
@@ -93,6 +93,14 @@ ActiveRecord::Schema.define(:version => 20130803160734) do
   create_table "messages", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "portfolios", :force => true do |t|
+    t.string   "title"
+    t.string   "picture"
+    t.text     "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
   create_table "posts", :force => true do |t|
