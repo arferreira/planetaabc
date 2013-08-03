506b51a15ee1f66f31b0de8313ff89681fdb6b4b
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-08-03 19:24:45 -0300

    controller novo

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,27 @@
 class CommentsController < InheritedResources::Base
+  def new
+    @comment = Comment.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @comment }
+    end
+  end
+
+  # POST /badges
+  # POST /badges.json
+  def create
+    @post = Post.find(params[:comment :post_id])
+    @comment = @post.comments.build(params[:comment])
+
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to @comment, notice: 'Badge was successfully created.' }
+        format.json { render json: @comment, status: :created, location: @comment }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ActiveRecord::Base
+  attr_accessible :user_name, :email, :site, :post_id
   belongs_to :post
-  attr_accessible :body, :post_id, :user_name, :email, :site
 end
