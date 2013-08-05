2debfcb08882baed9fad87bd0dac163496a7c57c
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-08-05 10:30:46 -0300

    portfolio migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130803224444) do
+ActiveRecord::Schema.define(:version => 20130805132733) do
 
   create_table "abouts", :force => true do |t|
     t.string   "title"
@@ -101,6 +101,7 @@ ActiveRecord::Schema.define(:version => 20130803224444) do
     t.text     "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.integer  "category_id"
   end
 
   create_table "post_comments", :force => true do |t|
