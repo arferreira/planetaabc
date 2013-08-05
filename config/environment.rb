8438b5187021fc4137dde997d38e0e41c6e8fe68
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-08-05 11:43:43 -0300

    data inicial e data final

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,4 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Planetaabc::Application.initialize!
-Time::DATE_FORMATS[:post] = "posted on %b %m, %Y"
+Time::DATE_FORMATS[:post] = "%b %m, %Y - %I:%M%p"
