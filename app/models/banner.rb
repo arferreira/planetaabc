bdbe4b94d93c959c658d03588b3484247bc8c280
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-08-06 14:39:47 -0300

    adicionando uploader no banner

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -1,4 +1,7 @@
 class Banner < ActiveRecord::Base
   mount_uploader :picture, PictureUploader
+  mount_uploader :icon1, Icon1Uploader
+  mount_uploader :icon3, Icon3Uploader
+  mount_uploader :icon2, Icon2Uploader
   attr_accessible :icon1, :icon2, :icon3, :name, :picture
 end
