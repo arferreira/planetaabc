8bfb8c39c63d46b554f463f6e292523e440d140a
Antonio Ricardo <antonioricardo@ifollowagencia.com.br>
2013-09-09 14:29:41 -0400

    deployyy

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,96 +33,26 @@ role :web, application
 role :app, application
 role :db,  application, primary: true
 
-
- 
-# Minhas configurações do Unicorn
-# comando para execução do unicorn
-# <optinal>
-set :unicorn_binary,  "bundle exec unicorn"
-# caminho para o arquivo de configuração do unicorn
-# <optinal>
-set :unicorn_config,  "#{current}/config/unicorn.rb"
-# onde será armazenado o pid do processo do unicorn
-# <optinal>
-set :unicorn_pid,     "#{deploy_to}/shared/pids/unicorn.pid"
-
-
-# executar antes do 'deploy:update_code' o comando 'deploy.check_folders'
-# do capistrano
-before 'deploy:update_code' do
-  deploy.check_folders
-end
-
-
-
-
 namespace :deploy do
+  task :start do
+    %w(config/database.yml).each do |path|
+      from  = "#{deploy_to}/#{path}"
+      to    = "#{current}/#{path}"
 
-  ## News methods ->
+      run "if [ -f '#{to}' ]; then rm '#{to}'; fi; ln -s #{from} #{to}"
+    end
 
-  #starta a aplicação com o unicorn
-  desc "Start Application"
-  task :start, :roles => :app, :except => { :no_release => true } do 
-    # comando bash, navega ate a pasta da versao atual, e executa o unicorn
-    run "cd #{current_path} && #{try_sudo} #{unicorn_binary} -c #{unicorn_config}" <<
-        " -E #{rails_env} -D"
+    run "cd #{current} && RAILS_ENV=production && GEM_HOME=/opt/local/ruby/gems && bundle exec unicorn_rails -c #{deploy_to}/config/unicorn.rb -D"
+
   end
-  
-  # mata o serviço do unicorn
-  desc "Stop Application"
-  task :stop, :roles => :app, :except => { :no_release => true } do 
-    # mata o serviço do unicorn passando o pid definido na linha 99
-     #run "#{try_sudo} kill `cat #{unicorn_pid}`"
-     run "if [ -e /var/www/planetaabc/shared/pids/unicorn.pid ]; then kill `cat /var/www/planetaabc/shared/pids/unicorn.pid`; fi;"
-     #run "if [ -e /var/www/planetaabc/shared/pids/unicorn.pid ]; then kill `cat /var/www/planetaabc/shared/pids/unicorn.pid`; fi;"
+
+  task :stop do
+   
+    run "if [ -e /var/www/ifollow/shared/pids/unicorn.pid ]; then kill `cat /var/www/ifollow/shared/pids/unicorn.pid`; fi;"
   end
-  
-  # mata o serviço do unicorn apos axecuções atual
-  desc "Graceful Stop Application"
-  task :graceful_stop, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} kill -s QUIT `cat #{unicorn_pid}`"
-  end
-  
-  # reinicia o serviço do unicorn
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    # para o serviço
+
+  task :restart do
     stop
-    # starta o serviço
     start
   end
-
-  # verifica as pasta necessarias para o envio, e inicialização do s serviços
-  # para corrigir bug que aconteceu comigo, talvez ja tenham corrigido esse erro
-  desc "Creating folders necessary"
-  task :check_folders do
-    run "if [ ! -d '#{deploy_to}' ];then mkdir #{deploy_to}; fi"
-    run "if [ ! -d '#{deploy_to}/#{version_dir}' ];then mkdir #{deploy_to}/#{version_dir}; fi"
-    run "if [ ! -d '#{deploy_to}/#{shared_dir}' ];then mkdir #{deploy_to}/#{shared_dir}; fi"
-    run "if [ ! -d '#{deploy_to}/#{shared_dir}/pids' ];then mkdir #{deploy_to}/#{shared_dir}/pids; fi"
-    run "if [ ! -d '#{deploy_to}/#{shared_dir}/log' ];then mkdir #{deploy_to}/#{shared_dir}/log; fi"
-  end
 end
-
-namespace :ruby do
-  desc "Show ruby version"
-  task :version do
-    run "cd #{current_release} && ruby -v"
-  end
-  desc "List process (sla) on server"
-  task :list do
-    run "top"
-  end
-end
-
-namespace :unicorn do
-  desc "Show error log"
-  task :error_log, :except => { :no_release => true } do
-    run "cat #{deploy_to}/#{shared_dir}/log/unicorn.stderr.log"
-  end
-
-  desc "Show out log"
-  task :out_log, :except => { :no_release => true } do
-    run "cat #{deploy_to}/#{shared_dir}/log/unicorn.stdout.log"
-  end
-end
-
