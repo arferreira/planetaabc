818289aa55c99b7e855bae23197868304c985295
Antonio Ricardo <antonioricardo@ifollowagencia.com.br>
2013-09-20 02:13:12 -0400

    Alteração layout e todos os componentes do site

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,24 +1,11 @@
 class ContactController < ApplicationController
 
   def new
-    @menu = true
-    @message = Message.new
+    #@menu = true
+   # @message = Message.new
     @banners = Banner.all
   end
 
   def create
-    @message = Message.new(params[:message])
-    if @message.valid?
 
-      if UserMailer.new_message(@message).deliver
-        UserMailer.new_message_confirmation(@message).deliver
-        redirect_to(request.path, :notice => "Seu e-mail fo enviado com sucesso, aguarde, que muito breve vamos te retornar!")
-      end
-      
-    else
-      flash.now.alert = "Por favor preencha todos os campos"
-      render :new
-    end
-    @banners = Banner.all
-  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,18 @@
     @services = Service.limit(3)
     @content = About.first
     @banners = Banner.all
+
+    # Feed do blog 
+
+    @feed_posts_blog = feed_blog
+
+    #final feed blog
+  end
+
+  def feed_blog
+    
+    posts = Post.limit(2)
+
   end
 
   def about
@@ -35,21 +47,7 @@
 
   def contact
   	# action contact
-   
-    @message = Message.new(params[:message])
-    if @message.valid?
-
-      if UserMailer.new_message(@message).deliver
-        UserMailer.new_message_confirmation(@message).deliver
-        redirect_to(request.path, :notice => "Seu e-mail fo enviado com sucesso, aguarde, que muito breve vamos te retornar!")
-      end
-      
-    else
-      flash.now.alert = "Por favor preencha todos os campos"
-      render :new
-    end
-    
-     @banners = Banner.all
+ 
   end
 
 end
